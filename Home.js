c3f8d6c3347a01990543cf5131f72408e977e287
CipherCircuit9261 <aprameyananantha@gmail.com>
2025-01-14 11:56:16 -0500

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,10 @@
+// Console log messages
+// Log the introduction with personal details
+console.log("I am CipherCircuit9261, I am 14 years old, and I am from America.");
+
+// Welcome message for visitors of the Mahabharata website
+console.log("Welcome to the Coding Club website.");
+
+console.log("I am the Developer, Programmer, and Designer of this team")
+
+
